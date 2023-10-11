a0486cfeba0cd2cdbb53ddf98ca87e9d040a461f
cheuni02 <inhcheung@gmail.com>
2023-10-11 16:07:34 +0100

    fix(core): not needed

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,6 @@ import MovieCard from "./components/MovieCard";
 
 const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_API_KEY}`;
 
-const movieSample = {
-  Title: "Spiderman in Cannes",
-  Year: "2016",
-  imdbID: "tt5978586",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg",
-};
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -23,7 +14,7 @@ const App = () => {
     setMovies(response.Search);
   };
   useEffect(() => {
-    searchMovies('spiderman');
+    searchMovies('');
   }, []);
   return (
     <div className="app">
